674e5caaf8a4f451c044135b1eeb0f48d8f3db6b
Alessio Linares <alessio.linares2@gmail.com>
2015-01-15 13:15:41 +0100

    Added release resource option

--- AudioPlayer.cpp
+++ AudioPlayer.cpp
@@ -5,6 +5,7 @@ AudioPlayer* AudioPlayer::s_audio_player = NULL;
 AudioPlayer::AudioPlayer()
 {
     m_music.setLoop(true);
+    m_resource_count = 0;
 }
 
 AudioPlayer* AudioPlayer::instance()
@@ -30,7 +31,7 @@ AudioPlayer::~AudioPlayer()
 
 bool AudioPlayer::playMusic(int i)
 {
-    if (!m_music.openFromFile(m_music_list[i]))
+    if (!m_music.openFromFile(m_music_table[i]))
         return false;
     m_music.setVolume(100);
     m_music.play();
@@ -39,7 +40,7 @@ bool AudioPlayer::playMusic(int i)
 
 bool AudioPlayer::playMusic(int i, float j)
 {
-    if (!m_music.openFromFile(m_music_list[i]))
+    if (!m_music.openFromFile(m_music_table[i]))
         return false;
     m_music.setVolume(j);
     m_music.play();
@@ -48,14 +49,14 @@ bool AudioPlayer::playMusic(int i, float j)
 
 void AudioPlayer::playSound(int i)
 {
-    m_sound.setBuffer(m_sound_list[i]);
+    m_sound.setBuffer(m_sound_table[i]);
     m_sound.setVolume(100);
     m_sound.play();
 }
 
 void AudioPlayer::playSound(int i, float j)
 {
-    m_sound.setBuffer(m_sound_list[i]);
+    m_sound.setBuffer(m_sound_table[i]);
     m_sound.setVolume(j);
     m_sound.play();
 }
@@ -70,18 +71,44 @@ void AudioPlayer::stopSound()
     m_sound.stop();
 }
 
-int AudioPlayer::addMusic(std::string path)
+int AudioPlayer::loadMusic(const std::string& path)
 {
-    m_music_list.push_back(path);
-    return m_music_list.size()-1;
+    auto it = m_resource_table.find(path);
+    int i;
+    if (it == m_resource_table.end())
+    {
+        i = m_resource_count++;
+        m_music_table.insert(std::pair<int, std::string>(i, path));
+        auto it = m_resource_table.insert(std::pair<std::string, std::pair<int, int>>(path, std::pair<int, int>(i, 0))).first;
+        m_reverse_resource_table.insert(std::pair<int, res_table_it>(i, it));
+    }
+    else
+    {
+        it->second.second++;
+        i = it->second.first;
+    }
+    return i;
 }
 
-int AudioPlayer::addSound(std::string path)
+int AudioPlayer::loadSound(const std::string& path)
 {
-    sf::SoundBuffer buffer;
-    buffer.loadFromFile(path);
-    m_sound_list.push_back(buffer);
-    return m_sound_list.size()-1;
+    auto it = m_resource_table.find(path);
+    int i;
+    if (it == m_resource_table.end())
+    {
+        i = m_resource_count++;
+        sf::SoundBuffer buffer;
+        buffer.loadFromFile(path);
+        m_sound_table.insert(std::pair<int, sf::SoundBuffer>(i, buffer));
+        auto it = m_resource_table.insert(std::pair<std::string, std::pair<int, int>>(path, std::pair<int, int>(i, 0))).first;
+        m_reverse_resource_table.insert(std::pair<int, res_table_it>(i, it));
+    }
+    else
+    {
+        it->second.second++;
+        i = it->second.first;
+    }
+    return i;
 }
 
 void AudioPlayer::setMusicOffset(sf::Time t)
@@ -92,4 +119,28 @@ void AudioPlayer::setMusicOffset(sf::Time t)
 sf::Time AudioPlayer::getMusicOffset()
 {
     return m_music.getPlayingOffset();
+}
+
+void AudioPlayer::releaseSound(int i)
+{
+    auto it = m_reverse_resource_table.find(i)->second;
+    it->second.second--;
+    if (it->second.second == 0)
+    {
+        m_sound_table.erase(i);
+        m_resource_table.erase(it->first);
+        m_reverse_resource_table.erase(i);
+    }
+}
+
+void AudioPlayer::releaseMusic(int i)
+{
+    auto it = m_reverse_resource_table.find(i)->second;
+    it->second.second--;
+    if (it->second.second == 0)
+    {
+        m_music_table.erase(i);
+        m_resource_table.erase(it);
+        m_reverse_resource_table.erase(i);
+    }
 }
--- AudioPlayer.hpp
+++ AudioPlayer.hpp
@@ -3,8 +3,10 @@
 
 #include <SFML/Audio.hpp>
 #include <SFML/System.hpp>
-#include <vector>
+#include <unordered_map>
 #include <string>
+
+typedef std::unordered_map<std::string, std::pair<int, int>>::iterator res_table_it;
 
 class AudioPlayer
 {
@@ -18,7 +20,7 @@ public:
     static void clear();
 
     // Add music resource to the player and returns its resource id
-    int addMusic(std::string path);
+    int loadMusic(const std::string& path);
     // Play music with resource id i
     bool playMusic(int i);
     // Play music with resource id i with volume 0 <= j <= 100
@@ -29,23 +31,30 @@ public:
     void setMusicOffset(sf::Time t);
     // Stop playing current music
     void stopMusic();
+    // Remove music resource
+    void releaseMusic(int i);
     
     // Add sound resource to the player and returns its resource id
-    int addSound(std::string path);
+    int loadSound(const std::string& path);
     // Play sound with resource id i
     void playSound(int i);
     // Play sound with resource id i with volume 0 <= j <= 100
     void playSound(int i, float j);
     // Stop playing current sound
     void stopSound();
+    // Remove sound resource
+    void releaseSound(int i);
 
 
 private:
     static AudioPlayer* s_audio_player;
-    std::vector<sf::SoundBuffer> m_sound_list;
-    std::vector<std::string> m_music_list;
+    int m_resource_count;
     sf::Sound m_sound;
     sf::Music m_music;
+    std::unordered_map<std::string, std::pair<int, int>> m_resource_table;
+    std::unordered_map<int, res_table_it> m_reverse_resource_table;
+    std::unordered_map<int, sf::SoundBuffer> m_sound_table;
+    std::unordered_map<int, std::string> m_music_table;
 };
 
 #endif
